ff9dcbb3e6e6b43777eb8c3bb1969fd2339f9e5a
K-J-HYEON <kjh950601@naver.com>
2022-10-28 18:40:48 +0900

    clear git cache

--- src/main/java/com/example/project/auth/infrastructure/entity/AuthRole.java
+++
@@ -1,5 +0,0 @@
-package com.example.project.auth.infrastructure.entity;
-
-public enum AuthRole {
-    ADMIN, USER
-}
--- src/main/java/com/example/project/auth/infrastructure/entity/AuthStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.auth.infrastructure.entity;
-
-public enum AuthStatus {
-    WITHDRAWL,
-    ACTIVE,
-    INACTIVE
-}
