f176c512676aae553be904b1e425c1110bf6ad72
K-J-HYEON <kjh950601@naver.com>
2022-10-31 14:40:33 +0900

    [update] update application-oauth.yml

--- src/main/java/com/example/project/auth/configuration/RedisConfiguration.java
+++ src/main/java/com/example/project/auth/configuration/RedisConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @Configuration
 @Slf4j
